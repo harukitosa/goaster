bab4592a5c55b2c94285fc7715963635b567d65b
harukitosa <13haruki28@gmail.com>
2020-09-04 14:20:06 +0900

    Dev:cmdディレクトリの作成

---
+++ cmd/goaster/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/harukitosa/goaster"
+
+func main() {
+	goaster.Run()
+}
--- goaster.go
+++ goaster.go
@@ -1,4 +1,4 @@
-package main
+package goaster
 
 import (
 	"bytes"
@@ -10,23 +10,32 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/harukitosa/goaster/ast"
-
 	"github.com/mattn/go-tty"
 )
 
-func initFlags() string {
+func initFlags() (string, bool) {
 	flag.Parse()
 	args := flag.Args()
+	writeCmd := false
 	filepath := ""
+	var cmd string
 	if len(args) == 1 {
 		filepath = args[0]
+	} else if len(args) == 2 {
+		filepath = args[0]
+		cmd = args[1]
 	} else {
 		fmt.Printf("Err:%s\n", errors.New("Please enter a file name"))
 		os.Exit(1)
 	}
-	return filepath
+
+	if cmd == "-w" {
+		writeCmd = true
+	}
+	return filepath, writeCmd
 }
 
 func openEditor(fileName string) error {
@@ -50,21 +59,44 @@ func openEditor(fileName string) error {
 	return nil
 }
 
-func main() {
-	filepath := initFlags()
+func fileName(filePath string) (string, error) {
+	if filePath == "" {
+		return "", errors.New("no value")
+	}
+	e := filepath.Base(filePath)
+	return e[0 : len(e)-3], nil
+}
+
+// Run starts goaster
+// 抽象構文木からいじって直せたらよさそう
+func Run() {
+	filepath, writeCmd := initFlags()
 	fset := token.NewFileSet()
 	expr, err := parser.ParseFile(fset, filepath, nil, 0)
 	if err != nil {
 		panic(err)
 	}
 	bf := bytes.NewBufferString("")
-	// ast.Fprint(bf, fset, expr, nil)
 	ast.Fprint(bf, fset, expr, nil)
-	f, err := os.Create(filepath + "ast")
+
+	var f *os.File
+	fname, err := fileName(filepath)
+	f, err = os.Create("ast-" + fname)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	defer func() {
 		f.Close()
+		if !writeCmd {
+			os.Remove(f.Name())
+		}
 	}()
 	f.WriteString(string(bf.Bytes()))
 	f.WriteString("\n")
-	openEditor(f.Name())
+
+	if !writeCmd {
+		openEditor(f.Name())
+	}
 }
